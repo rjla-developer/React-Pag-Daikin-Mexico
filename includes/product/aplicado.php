cd073adb1a3f7f0237f9550217be0aea6486977b
Alicia Monreal <amonreal@megcloud.mx>
2022-02-22 23:34:20 -0600

    Delete files or rename parent directory

--- includes/product/aplicado.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require '../functions.php';
-    includeTemplate('header', $inicio = true);
-    includeTemplate('modal');
-    include('aplicado_body.php');
-    includeTemplate('footer');
-?>
