cce6ada4313b2817ed079a9d1855b420b4ad976b
rjluengas <rjluengas@megcloud.mx>
2023-06-26 17:38:06 +0200

    Padding quitado para tener toda la pantalla

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,7 @@ import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 
 function Menu() {
   return (
-    <Container fluid className="fixed-top" id="home">
+    <Container fluid className="fixed-top px-0" id="home">
       {/* Menu Phone */}
       <Navbar
         className="d-block d-xl-none bg-gray shadow"
